97b9488e9c80e31c2464527447002aec73a79bf7
Michael Causey <michaelhcausey@gmail.com>
2020-05-23 19:10:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,7 +167,7 @@ def team_colors(team_name)
     team.each do |stats, value|
       binding.pry
       if stats == :colors
-        return value
+        return home_away[value]
       end
     end
   end
