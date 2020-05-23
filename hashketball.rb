9c8fab635be5ac477d8fde2004dfa84009141142
Michael Causey <michaelhcausey@gmail.com>
2020-05-23 19:47:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -186,7 +186,6 @@ def player_numbers(team_name)
     if team[:team_name] == team_name
       team.each do |stats, value|
         binding.pry
-        if stats == :players
       end
     end
   end
